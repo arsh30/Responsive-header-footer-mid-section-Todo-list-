d24dbde053776951d536178e56c38bcf384b1570
Arshdeep Singh <as3096734@gmail.com>
2023-02-22 13:32:33 +0530

    update 2 - after push data in an array

--- src/components/UserForm/UserForm.js
+++ src/components/UserForm/UserForm.js
@@ -1,27 +1,85 @@
 import "./userform.css";
 import React, { Component } from "react";
+import Table from "../Table/Table";
+
+// const dataContext = React.createContext();
 
 export default class UserForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      state: "",
+      contact: "",
+      items: [],
+    };
+  }
+  submitForm = (e) => {
+    e.preventDefault();
+
+    // console.log(this.state.name);
+    const newEntry = {
+      name: this.state.name,
+      state: this.state.state,
+      contact: this.state.contact,
+    };
+    // console.log("NEWeNRTY", newEntry);
+    this.setState({
+      items: [...this.state.items, newEntry],
+    });
+  };
+
   render() {
+    // console.log(this.state.name);
+    // console.log(this.state.state);
+    // console.log(this.state.contact);
+
+    console.log(this.state.items);
+    // console.log(newEntry);
+
+    // const {items} = this.state;
+
     return (
-      <form action="">
-        <div className="name">
-          <label htmlFor="name">Name: </label>
-          <input type="text" id="name" autoComplete="off" />
-        </div>
+      <>
+        <form onSubmit={this.submitForm}>
+          <div className="name">
+            <label htmlFor="name">Name: </label>
+            <input
+              type="text"
+              id="name"
+              autoComplete="off"
+              value={this.state.name}
+              onChange={(e) => this.setState({ name: e.target.value })}
+            />
+          </div>
 
-        <div className="state">
-          <label htmlFor="state">State: </label>
-          <input type="text" id="state" autoComplete="off" />
-        </div>
+          <div className="state">
+            <label htmlFor="state">State: </label>
+            <input
+              type="text"
+              id="state"
+              autoComplete="off"
+              value={this.state.state}
+              onChange={(e) => this.setState({ state: e.target.value })}
+            />
+          </div>
 
-        <div className="name">
-          <label htmlFor="contact">Contact: </label>
-          <input type="text" id="contact" autoComplete="off" />
-        </div>
+          <div className="name">
+            <label htmlFor="contact">Contact: </label>
+            <input
+              type="text"
+              id="contact"
+              autoComplete="off"
+              value={this.state.contact}
+              onChange={(e) => this.setState({ contact: e.target.value })}
+            />
+          </div>
 
-        <input type="submit" value="Submit" />
-      </form>
+          <input type="submit" value="Submit" />
+        </form>
+
+        {/* <Table data={items}/> */}
+      </>
     );
   }
 }
